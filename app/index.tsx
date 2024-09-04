1322d521c1f902902954af62356d74762450eb1c
Ryanhustler420 <gouravgupta840@gmail.com>
2024-09-04 13:29:56 +0530

    navigating to profile page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StatusBar, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View className="flex-1 bg-white items-center justify-center">
+      <StatusBar barStyle={"default"} />
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/profile" className="text-blue-500">Go to profile</Link>
     </View>
   );
 }
