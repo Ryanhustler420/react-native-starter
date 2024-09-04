037478d9246dba5dbd7cde0d42f4e192aa109aef
Ryanhustler420 <gouravgupta840@gmail.com>
2024-09-04 15:53:43 +0530

    added scroll view

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,16 @@
 import { Link } from "expo-router";
-import { SafeAreaView, StatusBar, Text, View } from "react-native";
+import { SafeAreaView, ScrollView, StatusBar, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <SafeAreaView className="bg-primary h-full">
-      <View className="flex-1 items-center justify-center">
-        <StatusBar barStyle={"default"} />
-        <Text className="text-3xl text-white font-plight">Appname</Text>
-        <Link href="/home" className="text-blue-500">Go to home</Link>
-      </View>
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className="flex-1 items-center justify-center">
+          <StatusBar barStyle={"default"} />
+          <Text className="text-3xl text-white font-plight">Appname</Text>
+          <Link href="/home" className="text-blue-500">Go to home</Link>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
