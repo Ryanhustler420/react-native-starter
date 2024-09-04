796cfdd51863f6b0f96694e0956b3831e8994e69
Ryanhustler420 <gouravgupta840@gmail.com>
2024-09-04 22:03:53 +0530

    using reducer to manage single state of a component

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useCallback, useReducer } from 'react'
+import { Text, SafeAreaView, Alert } from 'react-native'
+
+const initialState = { valid: false };
+const reducer = (state: any, action: any) => {
+  const { result } = action;
+  return { ...state, valid: result === undefined };
+}
 
 const SignIn = () => {
+  const [formState, dispatchFormState] = useReducer(reducer, initialState);
+
+  const onHandleInput = useCallback(() => {
+    dispatchFormState({ result: undefined });
+    Alert.alert("User has signed in");
+  }, []);
+
   return (
-    <View>
+    <SafeAreaView>
       <Text>SignIn</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
