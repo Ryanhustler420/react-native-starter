c41fd34a2423830f93a164736dd2b65834ccd239
Ryanhustler420 <gouravgupta840@gmail.com>
2024-09-04 15:49:38 +0530

    added tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,49 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
+import { Tabs } from 'expo-router'
 import React from 'react'
+import { Ionicons } from '@expo/vector-icons'
 
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: '#FFA001',
+        }}
+      >
+        <Tabs.Screen 
+          name="home" 
+          options={{
+            title: 'Home',
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons name='home-outline' color={color} size={24} />
+            ),
+          }}
+        />
+        <Tabs.Screen 
+          name="create" 
+          options={{
+            title: 'Create',
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons name='add-outline' color={color} size={24} />
+            ),
+          }}
+        />
+        <Tabs.Screen 
+          name="profile" 
+          options={{
+            title: 'Profile',
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons name='person-outline' color={color} size={24} />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
   )
 }
 
